68574b861472a064bc3352a8cd576e0e023f3369
James Ransome <jamesransome@msn.com>
2017-08-16 11:50:17 +0100

    error handling on test fail

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -1,10 +1,5 @@
-var note1 = new Note("hello");
-// var noteManager = new noteManager();
-var array = ["note1", "note2"]
 
-expect(array).toInclude("note1");
-expect(string).toInclude("x");
 
-it('creates a note', function(){
-  return expect(note1).toBeInstanceOf(Note);
+it('test', function(){
+  return expect("hello").toBeInstanceOf(Number);
 });
