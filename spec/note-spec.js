b40e9ee585150e554d7053575e16156cf51589d6
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-16 11:04:57 +0100

    adds test for equality

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -3,7 +3,6 @@ var note1 = new Note("hello");
 var array = ["note1", "note2"]
 
 expect(array).toInclude("note1");
-expect(string).toInclude("x");
 
 it('creates a note', function(){
   return expect(note1).toBeInstanceOf(Note);
