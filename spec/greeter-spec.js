0ea6915b284218ea23f8a0fffdaa55f7fd84ab43
James Ransome <jamesransome@msn.com>
2017-10-04 15:52:14 +0100

    update examples and README

--- spec/greeter-spec.js
+++ spec/greeter-spec.js
@@ -3,6 +3,10 @@ var expectedGreeting = "Hello, James!";
 var smallTalk = "so..."
 
 describe("Greeter", function(){
+  it("returns a string", function(){
+    return expect(Greeter(name)).toBeInstanceOf(String);
+  });
+
   it("greets someone by name", function(){
     return expect(Greeter(name)).toEqual(expectedGreeting);
   });
